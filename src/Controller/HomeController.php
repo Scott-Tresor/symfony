24a0ef3ae1ac7033158df420cc8321da8266ccf3
scott <scotttresor@gmail.com>
2020-07-30 00:08:57 +0200

    porter  du changement

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PinsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(PinsRepository $pins): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $pin = $pins->findAll();
+        return $this->render('home/index.html.twig', compact('pin'));
     }
 }
