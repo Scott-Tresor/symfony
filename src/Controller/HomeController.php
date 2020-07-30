7ea0a75f5637ea8e92aff47d3b7455ef36525e04
scott <scotttresor@gmail.com>
2020-07-30 09:39:46 +0200

    feat(crud): creation du crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,31 +3,101 @@
 namespace App\Controller;
 
 use App\Entity\Pins;
+use App\Form\PinType;
 use App\Repository\PinsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods="GET")
      * @param PinsRepository $pins
      * @return Response
+     * @author scotttresor <scotttresor@gmail.com>
      */
     public function index(PinsRepository $pins): Response
     {
-        $pin = $pins->findAll();
+        $pin = $pins->findBy([], ['createdAt' => 'DESC']);
         return $this->render('home/index.html.twig', compact('pin'));
     }
 
     /**
-     * @Route("/pins/{id<[0-9]+>}" name="detail")
+     * @Route("/pins/create", name="create", methods="GET|POST")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     * @author scotttresor <scotttresor@gmail.com>
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PinType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pin = $form->getData();
+            $em->persist($pin);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('home/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="detail", methods="GET")
      * @param Pins $pins
      * @return Response
+     * @author scotttresor <scotttresor@gmail.com>
      */
     public function show(Pins $pins): Response
     {
         return $this->render('home/show.index.twig', compact('pins'));
     }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/edit", name="edit", methods="GET|PUT")
+     * @param Pins $pins
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     * @author scotttresor <scotttresor@gmail.com>
+     */
+    public function edit(Pins $pins, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(PinType::class, $pins,['method'=> 'PUT']);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pin = $form->getData();
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('home/edit.index.twig',[
+            'pins' => $pins,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}/delete", name="delete", methods="GET|PUT")
+     * @param Pins $pins
+     * @param EntityManagerInterface $em
+     * @return Response
+     * @author scotttresor <scotttresor@gmail.com>
+     */
+    public function delete(Pins $pins, EntityManagerInterface $em): Response
+    {
+        $em->remove($pins);
+        $em->flush();
+        return $this->redirectToRoute('home');
+    }
+    
 }
--- src/Entity/Pins.php
+++ src/Entity/Pins.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Traits\TimeStampable;
 use App\Repository\PinsRepository;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -12,6 +13,8 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Pins
 {
+    use TimeStampable;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
@@ -29,15 +32,6 @@ class Pins
      */
     private $description;
 
-    /**
-     * @ORM\Column(type="datetime", options={"default":"CURRENT_TIMESTAMP"})
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\Column(type="datetime", options={"default":"CURRENT_TIMESTAMP"})
-     */
-    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -68,39 +62,4 @@ class Pins
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeInterface
-    {
-        return $this->createdAt;
-    }
-
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
-    {
-        $this->createdAt = $createdAt;
-
-        return $this;
-    }
-
-    public function getUpdatedAt(): ?\DateTimeInterface
-    {
-        return $this->updatedAt;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
-    {
-        $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    /**
-     * @ORM\PrePersist
-     * @ORM\PreUpdate
-     */
-    public function updateTimeStamps()
-    {
-        if ($this->getcreatedAt() === null) {
-            $this->setCreatedAt(new \DateTimeImmutable);
-        }
-        $this->setUpdatedAt(new \DateTimeImmutable);
-    }
 }
